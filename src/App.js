98fc63f916ab4b9801709b7b6fb7c420c8508752
Hansoo Chang <chang@socrate.jp>
2023-02-15 16:16:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
+import Location from "./components/Location"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      canvasWidth: 360*3,
+      canvasHeight: 180*3,
+    };
+    const getLocation = () => {
+      fetch('http://api.open-notify.org/iss-now.json')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({latitude: data.iss_position.latitude,
+                       longitude: data.iss_position.longitude});
+      });
+    }
+    const getLocationRegularly = () => {
+      setInterval(getLocation, 1000);
+    };
+    getLocationRegularly();
+  }
+
+  render() {
+    return(
+      <div className='tc' id="main_title">
+        <h1 className='f1 tc yellow'>The Location of the International Satellite</h1>
+        <Location longitude={this.state.longitude} latitude={this.state.latitude} 
+                  canvasWidth={this.state.canvasWidth} canvasHeight={this.state.canvasHeight} />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Location.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import "./Location.css"
+import {useRef, useEffect} from 'react';
+import worldMap from './world_map.png';
+
+function Location (props) {
+	const img = new Image();
+	img.src = worldMap;
+	const longitude = parseFloat(props.longitude);
+	const latitude = parseFloat(props.latitude);
+	const canvasWidth = props.canvasWidth;
+	const canvasHeight = props.canvasHeight;
+	const canvasRef = useRef(null);
+
+	function drawCircle(ctx, longitude, latitude) {
+		ctx.beginPath();
+		ctx.fillStyle = "red";
+		ctx.arc((180 + longitude)*canvasWidth/360, (90 - latitude)*canvasHeight/180, 10, 0, Math.PI*2, false);
+		ctx.fill();
+	}
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const ctx = canvas.getContext('2d');
+		ctx.fillStyle = "#00ffff";
+		ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvasWidth, canvasHeight);
+		// ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+		drawCircle(ctx, longitude, latitude);
+	});
+	return(
+		<div>
+			<canvas id="canvas" ref={canvasRef} width={canvasWidth} height={canvasHeight} />
+			<br/>Longitude: {longitude}
+			<br/>Latitude: {latitude}
+		</div>);
+}
+
+export default Location;
